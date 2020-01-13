566319f859fba5fb2f04d9abf0d51c559df2abf5
taweon-ted <taewon.ted@navercorp.com>
2020-01-13 23:26:38 +0900

    [Chap05] super & this
    - 차이점과 실행순서 예제

---
+++ src/chap5/section04/ThisTest.kt
@@ -0,0 +1,23 @@
+open class Person {
+    constructor(firstName: String) {
+        println("[person] firstName: $firstName")
+    }
+
+    constructor(firstName: String, age: Int) {
+        println("[person] firstName: $firstName, age: $age")
+    }
+}
+
+class Developer: Person {
+    constructor(firstName: String): this(firstName, 10) {
+        println("[developer] $firstName, $age")
+    }
+
+    constructor(firstName: String, age: Int): super(firstName, age) {
+        println("[developer] $firstName, $age")
+    }
+}
+
+fun main() {
+    val sean = Developer("Sean")
+}
