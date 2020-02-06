4b74cb5e9ac6e777c11c55e3b4215ebeb1a1040e
taewon-ted <taewon.ted@navercorp.com>
2020-02-06 21:09:10 +0900

    [chap07] abstract object

---
+++ src/chap7/section01/AbstractObjectTest.kt
@@ -0,0 +1,15 @@
+package chap7.section01
+
+abstract class Printer {
+    abstract fun print()
+}
+
+val myPrinter = object : Printer() {
+    override fun print() {
+        print("출력합니다.")
+    }
+}
+
+fun main() {
+    myPrinter.print()
+}
