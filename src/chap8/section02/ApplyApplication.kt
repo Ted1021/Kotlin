eea3e66c15802d8d5f3dc4038ded3afdae0afd9c
taewon-ted <taewon.ted@navercorp.com>
2020-02-24 17:21:54 +0900

    [chap8] array apply
    - plus & slice

---
+++ src/chap8/section02/ApplyApplication.kt
@@ -0,0 +1,21 @@
+package chap8.section02
+
+import java.util.*
+
+fun main() {
+
+}
+
+fun plusAndSlice() {
+    // array 는 기본적으로 한번 생성되고나면,
+    // 크기가 메모리영역에 고정되게되어 이후 늘리거나 줄일 수 없다.
+    val arr1 = arrayOf(1,2,3,4,5)
+
+    // plus 와 slice 와 같은 내부메소드를 제공하는데,
+    // 이는 본래의 배열의 크기가 변경되는것이 아닌
+    // 새롭게 만들어지는 배열에 덮어씌우는 형태이다.
+    val arr2 = arr1.plus(6)
+    println("arr2 : ${Arrays.toString(arr2)}")
+    val arr3 = arr1.sliceArray(0..3)
+    println("arr3 : ${Arrays.toString(arr3)}")
+}
